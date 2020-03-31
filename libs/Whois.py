3a619303849eec7bc5b92a5fda0ebe56d2b7d3a6
Piaaaa06 <mardayeestelle@gmail.com>
2020-03-31 16:40:14 +0200

    Bugs fixed.

--- libs/Whois.py
+++ libs/Whois.py
@@ -8,10 +8,12 @@ import re
 import subprocess
 
 #Regex1 for the function getWhois.
-regex1 = re.compile(r"^([\w\- ]+):\s*([\w\-: ]+)")
+#regex1 = re.compile(r"^[\s]*([\w\- ]+):\s*([\w\-: ]+)")
+regex1 = re.compile(r"^[\s]*([\w\- ]+):\s*([\w\-:. \/]+)")
+regex2 = re.compile(r"^([\w\- ]+):\s*([\w\-: ]+)")
 
 #Regex2 for the expiry date in the function getWhois (2021-01-09T18:56:18Z --> 2021-01-09).
-regex2 = re.compile(r"^(\d*-\d*-\d*)[\w][\d]*[:][\d]*[:][\d]*[\w]$")
+regex3 = re.compile(r"^(\d*-\d*-\d*)[\w][\d]*[:][\d]*[:][\d]*[\w]$")
 
 def getWhois(domain):
     info = {}
@@ -22,22 +24,45 @@ def getWhois(domain):
     if out.count("No entries found") > 0:
         return info
 
+    #Two types of output exists with the Whois command : paraf0 treats one of them and paraf1, the other one.
+
+    paraf0 = out.split("\n\n")[0]
     # Only the first paragraphe of the result.
     paraf1 = out.split("\n\n")[1]
 
-    for l in paraf1.split("\n"):
-        m = regex1.match(l)
-        if m != None:
-            g = m.groups()
-            info[g[0]]=g[1]
+    #"Domain Name:" only exists in the first type of output.
+    if paraf0.count("Domain Name:") > 0:
+        for l in paraf0.split("\n"):
+            m = regex1.match(l)
+            if m != None:
+                g = m.groups()
+                info[g[0]]=g[1] 
 
-    m = regex2.match(info["Expiry Date"])
-    if m != None:
-        info["Expiry Date"] = m.groups()[0]
+        if "Registry Expiry Date" in info:
+            m = regex3.match(info["Registry Expiry Date"])
+            if m != None:
+                info["Registry Expiry Date"] = m.groups()[0]
 
-    print("\t\tRegistrar : ", info["registrar"])
-    print("\t\tSource : ", info["source"])
-    print("\t\tExpiry date : ", info["Expiry Date"])
+        print("\t\tRegistrar : ", info["Registrar"])
+        print("\t\tExpiry date : ", info["Registry Expiry Date"])
+
+
+    #"hold:" only exists in the second type of output.
+    elif paraf1.count("hold:") > 0:
+        for l in paraf1.split("\n"):
+            m = regex2.match(l)
+            if m != None:
+                g = m.groups()
+                info[g[0]]=g[1]
+
+        if "Expiry Date" in info:
+            m = regex3.match(info["Expiry Date"])
+            if m != None:
+                info["Expiry Date"] = m.groups()[0]
+
+        print("\t\tRegistrar : ", info["registrar"])
+        print("\t\tSource : ", info["source"])
+        print("\t\tExpiry date : ", info["Expiry Date"])
 
 
 
@@ -60,3 +85,4 @@ def getHost(company_domain):
 
     for i in range(len(ip_adresses)):
         print("\t\tIP adress ", str(i+1), " : ", ip_adresses[i])
+
